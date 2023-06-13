e94a1a77bc77014ee42bfc69e463cd9883525e4f
epignatelli <edu.pignatelli@gmail.com>
2023-06-13 22:15:35 +0100

    add copyright and bump version

--- navix/actions.py
+++ navix/actions.py
@@ -1,3 +1,23 @@
+# Copyright 2023 The Navix Authors.
+
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+
+#   http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+
 from __future__ import annotations
 
 
@@ -36,7 +56,7 @@ def _move(state: State, entity_id: int, direction: int) -> State:
         legal,
         lambda grid: jnp.where(new_mask, entity_id, remove_entity(grid, entity_id)),
         lambda grid: grid,
-        state.grid
+        state.grid,
     )
     return state.replace(grid=grid)
 
