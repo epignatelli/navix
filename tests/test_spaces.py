7f2713b9a48daabca5f5bba1d15996f666518c79
“Eduardo <edu.pignatelli@gmail.com>
2023-07-19 09:37:23 +0100

    test(spaces): add a test for spaces

---
+++ tests/test_spaces.py
@@ -0,0 +1,34 @@
+import jax
+import jax.numpy as jnp
+from navix.spaces import Space, Continuous, Discrete, MAX_INT, MIN_INT
+
+
+def test_discrete():
+    key = jax.random.PRNGKey(42)
+    elements = (5, 0, MAX_INT, MIN_INT)
+    shapes = ((), (0,), (0, 0), (1, 2), (5, 5))
+    dtypes = (jnp.int8, jnp.int16, jnp.int32)
+    for element in elements:
+        for shape in shapes:
+            for dtype in dtypes:
+                space = Discrete(element, shape, dtype)
+                sample = space.sample(key)
+                print(sample)
+                assert jnp.all(jnp.logical_not(jnp.isnan(sample)))
+
+
+def test_continuous():
+    key = jax.random.PRNGKey(42)
+    shapes = ((), (0,), (0, 0), (1, 2), (5, 5))
+    min_max = [(0.0, 1.0), (0.0, 1), (0, 1), (1.0, -1.0), (MIN_INT, MAX_INT),]
+    for shape in shapes:
+        for minimum, maximum in min_max:
+            space = Continuous(shape=shape, minimum=jnp.asarray(minimum), maximum=jnp.asarray(maximum))
+            sample = space.sample(key)
+            print(sample)
+            assert jnp.all(jnp.logical_not(jnp.isnan(sample)))
+
+
+if __name__ == '__main__':
+    test_discrete()
+    test_continuous()
