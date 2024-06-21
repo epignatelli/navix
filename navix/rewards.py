54b5dc2235a16cd91c7c6a6d96b751f08ff522f7
epignatelli <edu.pignatelli@gmail.com>
2024-06-21 16:45:13 +0100

    docs(docstrings): include docstrings for rewards, spaces, states, terminations, transitions

--- navix/rewards.py
+++ navix/rewards.py
@@ -31,6 +31,19 @@ def compose(
     *reward_functions: Callable[[State, Array, State], Array],
     operator: Callable = jnp.sum,
 ) -> Callable:
+    """Compose multiple reward functions into a single reward function.
+    The functions are called in order and the results are reduced using the `operator` \
+    function.
+    
+    Args:
+        *reward_functions (Callable[[State, Array, State], Array]): A list of reward functions.
+        operator (Callable): The operator to reduce the results of the reward functions.
+        It must be a function that takes a list of arrays, or an array and returns an \
+        array of size `f32[]`.
+    
+    Returns:
+        Callable: A composed reward function that applies the `operator` to the results of the \
+        reward functions."""
     return lambda prev_state, action, state: operator(
         jnp.asarray(
             [f(prev_state, action, state) for f in reward_functions], dtype=jnp.float32
@@ -39,23 +52,62 @@ def compose(
 
 
 def free(state: State) -> Array:
+    """A reward function that always returns 0, to simulate reward-free learning.
+
+    Args:
+        state (State): The current state of the game.
+
+    Returns:
+        Array: A scalar array `f32[]` with value 0."""
     return jnp.asarray(0.0, dtype=jnp.float32)
 
 
 def on_goal_reached(prev_state: State, action: Array, state: State) -> Array:
+    """A reward function that returns 1 when the goal is reached, and 0 otherwise.
+
+    Args:
+        state (State): The current state of the game.
+
+    Returns:
+        Array: A scalar array `f32[]` with value 1 if the goal is reached, and 0 otherwise.
+    """
     return jnp.asarray(events.on_goal_reached(state), dtype=jnp.float32)
 
 
 def action_cost(
     prev_state: State, action: Array, new_state: State, cost: float = 0.01
 ) -> Array:
+    """A reward function that returns a negative value when an action is taken. 
+    All actions have a cost of `cost`, except for noops.
+    
+    Args:
+        prev_state (State): The previous state of the game.
+        action (Array): The action taken.
+        new_state (State): The new state of the game.
+        cost (float): The cost of taking an action.
+    
+    Returns:
+        Array: A scalar array `f32[]` with value -`cost` if the action is not a noop, \
+        and 0 otherwise."""
     # noops are free
-    return -jnp.asarray(action > 0, dtype=jnp.float32) * cost
+    return -jnp.asarray(action != 6, dtype=jnp.float32) * cost
 
 
 def time_cost(
     prev_state: State, action: Array, new_state: State, cost: float = 0.01
 ) -> Array:
+    """A reward function that returns a negative value as time passes, paying a cost \
+    of `cost` at each time step.
+
+    Args:
+        prev_state (State): The previous state of the game.
+        action (Array): The action taken.
+        new_state (State): The new state of the game.
+        cost (float): The cost of time passing.
+    
+    Returns:
+        Array: A scalar array `f32[]` with value -`cost`.
+    """
     # time always has a cost
     return -jnp.asarray(cost, dtype=jnp.float32)
 
@@ -63,13 +115,32 @@ def time_cost(
 def wall_hit_cost(
     prev_state: State, action: Array, state: State, cost: float = 0.01
 ) -> Array:
+    """A reward function that returns a negative value when the agent hits a wall, \
+    paying a cost of `cost` for each wall hit.
+    
+    Args:
+        state (State): The current state of the game.
+        cost (float): The cost of hitting a wall.
+    
+    Returns:
+        Array: A scalar array `f32[]` with value -`cost` if the agent hits a wall, \
+        and 0 otherwise."""
     return jnp.asarray(events.on_wall_hit(state), dtype=jnp.float32) * cost
 
 
-def on_door_done(
-    prev_state: State, action: Array, state: State, cost: float = 0.01
-) -> Array:
+def on_door_done(prev_state: State, action: Array, state: State) -> Array:
+    """A reward function that returns a positive value when the agent uses the action \
+    `done` in front of a door.
+    
+    Args:
+        state (State): The current state of the game.
+        
+    Returns:
+        Array: A scalar array `f32[]` with value 1 if the agent uses the action `done` in \
+        front of a door, and 0 otherwise."""
+
     return jnp.asarray(events.on_door_done(state), dtype=jnp.float32)
 
 
 DEFAULT_TASK = compose(on_goal_reached, action_cost)
+"""The default task for the game, composed of the `on_goal_reached` and `action_cost` reward functions."""
--- navix/terminations.py
+++ navix/terminations.py
@@ -32,29 +32,85 @@ def compose(
     *term_functions: Callable[[State, Array, State], Array],
     operator: Callable = jnp.any,
 ) -> Callable:
+    """Compose termination functions into a single termination function.
+
+    Args:
+        *term_functions (Callable): List of termination functions.
+        operator (Callable): Operator to combine the termination functions.
+
+    Returns:
+        Callable: A single termination function."""
     return lambda prev_state, action, state: operator(
         jnp.asarray([term_f(prev_state, action, state) for term_f in term_functions])
     )
 
 
 def check_truncation(terminated: Array, truncated: Array) -> Array:
+    """Check if the episode is truncated or terminated, and returns a value
+    that conforms to the `StepType` enum.
+
+    Args:
+        terminated (Array): A boolean array indicating whether the episode is terminated.
+        truncated (Array): A boolean array indicating whether the episode is truncated.
+
+    Returns:
+        Array: An integer array that represents the step type."""
     result = jnp.asarray(truncated + 2 * terminated, dtype=jnp.int32)
     return jnp.clip(result, 0, 2)
 
 
 def on_goal_reached(prev_state: State, action: Array, state: State) -> Array:
+    """Check if the goal has been reached using the `goal_reached` event.
+
+    Args:
+        prev_state (State): The previous state of the game.
+        action (Array): The action taken by the player.
+        state (State): The current state of the game.
+
+    Returns:
+        Array: A boolean array indicating whether the goal has been reached."""
     return jnp.asarray(events.on_goal_reached(state), dtype=jnp.bool_)
 
 
 def on_lava_fall(prev_state: State, action: Array, state: State) -> Array:
+    """Check if the lava has fallen using the `lava_fall` event.
+
+    Args:
+        prev_state (State): The previous state of the game.
+        action (Array): The action taken by the player.
+        state (State): The current state of the game.
+
+    Returns:
+        Array: A boolean array indicating whether the lava has fallen."""
     return jnp.asarray(events.on_lava_fall(state), dtype=jnp.bool_)
 
 
 def on_ball_hit(prev_state: State, action: Array, state: State) -> Array:
+    """Check if the ball has hit something using the `ball_hit` event.
+
+    Args:
+        prev_state (State): The previous state of the game.
+        action (Array): The action taken by the player.
+        state (State): The current state of the game.
+
+    Returns:
+        Array: A boolean array indicating whether the ball has hit something."""
     return jnp.asarray(events.on_ball_hit(state), dtype=jnp.bool_)
 
 
 def on_door_done(prev_state: State, action: Array, state: State) -> Array:
+    """Check if the action `done` has been called in front of a `Door` object with the \
+        correct colour.
+
+    Args:
+        prev_state (State): The previous state of the game.
+        action (Array): The action taken by the player.
+        state (State): The current state of the game.
+
+    Returns:
+        Array: A boolean array indicating whether the action `done` has been called in \
+        front of a `Door` object with the correct colour.
+    """
     return jnp.asarray(events.on_door_done(state), dtype=jnp.bool_)
 
 
