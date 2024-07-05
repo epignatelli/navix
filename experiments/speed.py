d8b7a9bef5bc03ee3dc666d179072c1a68e74432
epignatelli <edu.pignatelli@gmail.com>
2024-07-05 15:11:08 +0100

    add speedup experiments

--- experiments/speed.py
+++ experiments/speed.py
@@ -1,9 +1,17 @@
 import os
+import time
 import timeit
 import json
 import gymnasium as gym
 import jax
+import jax.numpy as jnp
+from matplotlib import pyplot as plt
 import navix as nx
+
+
+jax.config.update("jax_compilation_cache_dir", "/tmp/jax_cache")
+jax.config.update("jax_persistent_cache_min_entry_size_bytes", 4)
+jax.config.update("jax_persistent_cache_min_compile_time_secs", 1)
 
 
 def run_minigrid(env_id: str, num_steps: int, num_runs: int):
@@ -18,25 +26,9 @@ def run_minigrid(env_id: str, num_steps: int, num_runs: int):
             timestep = env.step(action)
         return timestep
 
-    times = timeit.timeit(_run, number=num_runs)
-    return times
-
-
-def run_navix_jit_step(env_id: str, num_steps: int, num_runs: int):
-    print("Running Navix JIT step...")
-    env = nx.make(env_id, max_steps=num_steps)
-
-    key = jax.random.PRNGKey(0)
-    timestep = env.reset(key)
-    step = jax.jit(env.step).lower(timestep, env.action_space.sample(key)).compile()
-
-    def _run(timestep):
-        for i in range(num_steps):
-            action = env.action_space.sample(key)
-            timestep = step(timestep, action)
-        return timestep
-
-    times = timeit.timeit(lambda: _run(timestep).t.block_until_ready(), number=num_runs)
+    times = timeit.repeat(_run, number=1, repeat=num_runs)
+    _run()
+    print(f"Time taken for one run: {times} seconds")
     return times
 
 
@@ -52,20 +44,44 @@ def run_navix_jit_loop(env_id: str, num_steps: int, num_runs: int):
             timestep = env.step(timestep, action)  # Update the environment state
             return timestep, ()
 
-        return jax.lax.scan(body_fun, timestep, actions, unroll=10)[0]
+        return jax.lax.scan(body_fun, timestep, actions, unroll=20)[0]
 
     key = jax.random.PRNGKey(0)
-    run = jax.jit(_run).lower(key).compile()
-    times = timeit.timeit(lambda: run(key).t.block_until_ready(), number=num_runs)
+    _run = jax.jit(_run).lower(key).compile()
+    times = timeit.repeat(
+        lambda: _run(key).t.block_until_ready(), number=1, repeat=num_runs
+    )
+    print(f"Time taken for one run: {times} seconds")
     return times
+
+
+def speedup_by_num_steps():
+    print("*" * 80)
+    print("Running speedup by num steps")
+    print("*" * 80)
+    NUM_RUNS = 5
+    ENV_ID = "Navix-Empty-8x8-v0"
+    gym_env_id = ENV_ID.replace("Navix", "MiniGrid")
+
+    results = {}
+    for order in range(1, 7):
+        num_steps = 10**order
+        print(num_steps)
+        results[num_steps] = {
+            "minigrid": run_minigrid(gym_env_id, num_steps, NUM_RUNS),
+            "navix_jit_loop": run_navix_jit_loop(ENV_ID, num_steps, NUM_RUNS),
+        }
+        with open(
+            os.path.join(os.path.dirname(__file__), "speedup_num_steps.json"), "w"
+        ) as f:
+            json.dump(results, f, indent=2)
 
 
 def speedup_by_env():
     print("*" * 80)
     print("Running speedup by env...")
     print("*" * 80)
-    # NUM_STEPS = 1_000
-    NUM_STEPS = 10
+    NUM_STEPS = 1_000
     NUM_RUNS = 1
 
     results = {}
@@ -75,7 +91,6 @@ def speedup_by_env():
             print(env_id)
             results[env_id] = {
                 "minigrid": run_minigrid(gym_env_id, NUM_STEPS, NUM_RUNS),
-                "navix_jit_step": run_navix_jit_step(env_id, NUM_STEPS, NUM_RUNS),
                 "navix_jit_loop": run_navix_jit_loop(env_id, NUM_STEPS, NUM_RUNS),
             }
             with open(
@@ -88,29 +103,111 @@ def speedup_by_env():
             print(f"Error in {env_id}: {repr(e)}")
 
 
-def speedup_by_num_steps():
-    print("*" * 80)
-    print("Running speedup by num steps")
-    print("*" * 80)
-    NUM_RUNS = 1
-    ENV_ID = "Navix-Empty-8x8-v0"
-    gym_env_id = ENV_ID.replace("Navix", "MiniGrid")
+def plot_speedup_by_num_steps():
+    with open(
+        os.path.join(os.path.dirname(__file__), "speedup_num_steps.json"), "r"
+    ) as f:
+        results = json.load(f)
+    minigrid_times = {k: v["minigrid"] for k, v in results.items()}
+    navix_times = {k: v["navix_jit_loop"] for k, v in results.items()}
+    fig, ax = plt.subplots(figsize=(11, 4), dpi=150)
+    xs_minigrid = [int(x) for x in minigrid_times.keys()]
+    ys_minigrid = jnp.asarray(list(minigrid_times.values()))
+    print(ys_minigrid)
+    ax.errorbar(
+        xs_minigrid,
+        ys_minigrid.mean(axis=-1),
+        label="MiniGrid",
+        yerr=ys_minigrid.std(axis=-1),
+        color="black",
+        marker="o",
+    )
+    xs_navix = [int(x) for x in navix_times.keys()]
+    ys_navix = jnp.asarray(list(navix_times.values()))
+    ax.errorbar(
+        xs_navix,
+        ys_navix.mean(axis=-1),
+        yerr=ys_navix.std(axis=-1),
+        label="NAVIX",
+        color="red",
+        marker="s",
+    )
+    ax.set_xlabel("Number of steps", fontsize=12)
+    ax.set_ylabel("Time (s)", fontsize=12)
+    ax.set_title("Speed up by number of steps", fontsize=14)
+    ax.tick_params(axis="both", which="major", labelsize=10)
+    ax.set_xlabel("Number of steps")
+    ax.set_ylabel("Time (s)")
+    ax.set_title("Speed up by number of steps")
+    ax.set_yscale("log")
+    ax.set_xscale("log")
+    ax.grid(axis="y", linestyle=(0, (6, 8)), alpha=0.6)
+    legend = fig.legend(
+        loc="lower center",
+        ncol=2,
+        bbox_to_anchor=(0.53, -0.1),  # Adjust the y-coordinate to add more white space
+        shadow=False,
+        frameon=False,
+    )
+    fig.savefig(
+        os.path.join(os.path.dirname(__file__), "speedup_num_steps.png"),
+        bbox_extra_artists=(legend,),
+        bbox_inches="tight",
+    )
 
-    results = {}
-    for order in range(1, 7):
-        num_steps = 10**order
-        print(num_steps)
-        results[num_steps] = {
-            "minigrid": run_minigrid(gym_env_id, num_steps, NUM_RUNS),
-            "navix_jit_step": run_navix_jit_step(ENV_ID, num_steps, NUM_RUNS),
-            "navix_jit_loop": run_navix_jit_loop(ENV_ID, num_steps, NUM_RUNS),
-        }
-        with open(
-            os.path.join(os.path.dirname(__file__), "speedup_num_steps.json"), "w"
-        ) as f:
-            json.dump(results, f, indent=2)
+
+def plot_speedup_by_env():
+    with open(os.path.join(os.path.dirname(__file__), "speedup_env.json"), "r") as f:
+        results = json.load(f)
+    minigrid_times = {k: v["minigrid"] for k, v in results.items()}
+    navix_times = {k: v["navix_jit_loop"] for k, v in results.items()}
+    fig, ax = plt.subplots(figsize=(11, 4), dpi=150)
+    xs = range(len(minigrid_times))
+    ys_minigrid = jnp.asarray(list(minigrid_times.values()))
+    ys_navix = jnp.asarray(list(navix_times.values()))
+    ax.bar(
+        [x - 0.2 for x in xs],
+        ys_minigrid,
+        label="MiniGrid",
+        color="black",
+        width=0.4,
+    )
+    ax.bar(
+        [x + 0.2 for x in xs],
+        ys_navix,
+        label="NAVIX",
+        color="red",
+        alpha=0.7,
+        width=0.4,
+    )
+    ax.set_xlabel("Number of steps", fontsize=12)
+    ax.set_ylabel("Time (s)", fontsize=12)
+    ax.set_title("Speed up by number of steps", fontsize=14)
+    ax.tick_params(axis="both", which="major", labelsize=10)
+    ax.set_xlabel("Environment")
+    ax.set_ylabel("Time (s)")
+    ax.set_title("Speed up by environment")
+    ax.set_xticks(xs)
+    ax.grid(axis="y", linestyle=(0, (6, 8)), alpha=0.6)
+    legend = fig.legend(
+        loc="lower center",
+        ncol=2,
+        bbox_to_anchor=(0.53, -0.1),
+        shadow=False,
+        frameon=False,
+    )
+    fig.tight_layout()
+    fig.savefig(
+        os.path.join(os.path.dirname(__file__), "speedup_env.png"),
+        bbox_extra_artists=(legend,),
+        bbox_inches="tight",
+    )
+    print(ys_navix, ys_minigrid, ys_minigrid / ys_navix)
+    print(jnp.mean(ys_minigrid / ys_navix), jnp.std(ys_minigrid / ys_navix))
 
 
 if __name__ == "__main__":
-    speedup_by_num_steps()
-    speedup_by_env()
+    # speedup_by_num_steps()
+    # speedup_by_env()
+    plot_speedup_by_num_steps()
+    plot_speedup_by_env()
