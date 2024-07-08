11599b11fc632a2d354f9ce9074d4584a9d48980
epignatelli <edu.pignatelli@gmail.com>
2024-07-08 10:10:43 +0100

    formatting

--- tests/test_issues.py
+++ tests/test_issues.py
@@ -1,3 +1,22 @@
+# Copyright 2023 The Navix Authors.
+
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+
+#   http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
 from __future__ import annotations
 
 import os
@@ -12,6 +31,7 @@ import matplotlib.pyplot as plt
 
 def test_82():
     os.environ["JAX_OMNISTAGING"] = "0"
+
     def render(timestep):
         plt.imshow(timestep.observation)
         plt.axis(False)
@@ -30,7 +50,7 @@ def test_82():
     # . # . #
     # K D G #
     # # # # #
-    
+
     # start agent direction = EAST
     prev_pos = timestep.state.entities["player"].position
     # action 2 is forward
